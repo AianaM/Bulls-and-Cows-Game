83135a3522af74d877ab51e5efb75e6ac7184159
Aiana <Aiana.Miachina@gmail.com>
2022-04-08 21:21:55 +0900

    Initial Commit

---
+++ Bulls and Cows Game/Bulls_and_Cows_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bulls_and_Cows_GameApp.swift
+//  Bulls and Cows Game
+//
+//  Created by Aiana Miachina on 08.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Bulls_and_Cows_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
